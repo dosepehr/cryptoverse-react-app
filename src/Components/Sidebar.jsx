04242b5d77bec0ead27b1a8f7008e175b116988c
dosepehr <dosepehr@gmail.com>
2023-03-02 05:00:17 +0330

    make sidebar navlink functionality

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
     return (
-        <div className='h-screen bg-blue-900 w-60 p-4'>
-            <div className='flex items-center justify-center space-x-3'>
+        <div className='h-screen bg-blue-900 w-60 '>
+            <div className='flex items-center justify-center space-x-3 p-4'>
                 <img
-                    src='/public/assets/cryptocurrency.png'
+                    src='/assets/cryptocurrency.png'
                     alt='logo'
                     className='h-16'
                 />
-                <p className='text-2xl font-semibold text-blue-600 '>Cryptoverse</p>
+                <p className='text-2xl font-semibold text-blue-600 '>
+                    Cryptoverse
+                </p>
+            </div>
+            <div className='flex flex-col mt-11 space-y-3'>
+                <NavLink
+                    to='Home'
+                    className={({ isActive }) =>
+                        isActive ? 'activeLink' : 'notActiveLink'
+                    }
+                >
+                    <span className='hover:text-blue-900 transition-all duration-300'>
+                        Home
+                    </span>
+                </NavLink>
+                <NavLink
+                    to='Cryptocurrencies'
+                    className={({ isActive }) =>
+                        isActive ? 'activeLink' : 'notActiveLink'
+                    }
+                >
+                    <span className='hover:text-blue-900 transition-all duration-300'>
+                        Cryptocurrencies
+                    </span>
+                </NavLink>
+                <NavLink
+                    to='News'
+                    className={({ isActive }) =>
+                        isActive ? 'activeLink' : 'notActiveLink'
+                    }
+                >
+                    <span className='hover:text-blue-900 transition-all duration-300'>
+                        News
+                    </span>
+                </NavLink>
             </div>
         </div>
     );
