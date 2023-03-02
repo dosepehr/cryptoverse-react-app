df7bbaa52d572b6519c44e593f0b0622457966df
dosepehr <dosepehr@gmail.com>
2023-03-02 15:00:51 +0330

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { Sidebar } from './Components';
-
+import { Sidebar, Home, News, Cryptocurrencies } from './Components';
+import { Routes, Route } from 'react-router-dom';
 const App = () => {
     return (
-        <Sidebar />
-        
-    )
+        <>
+            <Sidebar />
+            <Routes>
+                <Route path='/Home' element={<Home />} />
+                <Route path='/News' element={<News />} />
+                <Route
+                    path='/Cryptocurrencies'
+                    element={<Cryptocurrencies />}
+                />
+            </Routes>
+        </>
+    );
 };
 
 export default App;
