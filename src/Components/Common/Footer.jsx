003ee568de139d7b24c2708df6bb6b1d08fb241b
dosepehr <dosepehr@gmail.com>
2023-03-02 15:24:41 +0330

    fix footer links

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -4,10 +4,14 @@ const Footer = () => {
     return (
         <div className='w-full bg-primaryColor h-[15vh] text-center text-white py-3'>
             <p>Cryptoverse</p>
-            <p className='my-3' >All rights reserverd</p>
+            <p className='my-3'>All rights reserverd</p>
             <div className='flex justify-center space-x-4 text-secondaryColor'>
-                <Link className='hover:text-white duration-300' >Home</Link>
-                <Link className='hover:text-white duration-300' >News</Link>
+                <Link className='hover:text-white duration-300' to='/Home'>
+                    Home
+                </Link>
+                <Link className='hover:text-white duration-300' to='/News'>
+                    News
+                </Link>
             </div>
         </div>
     );
